f2b32e50154f901f7443e7fbecb9ddc3fca31450
mendezfreitez <mendezfreitez@gmail.com>
2024-09-10 16:18:13 -0300

    Primer commit de prueba

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <M5StickCPlus2.h>
+
+void setup()
+{
+  M5.begin();
+  M5.Lcd.setTextSize(1.1);
+  M5.Lcd.setRotation(1);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setBrightness(30);
+}
+
+void loop()
+{
+  M5.Lcd.setCursor(3,30);
+  M5.Lcd.println("Esto es una prueba");
+  delay(3000);
+}
