9ba731ed506d950724d0f3a8038c5e700c382b21
mendezfreitez <mendezfreitez@gmail.com>
2024-10-03 00:32:31 -0300

    Agregar libreria.

--- src/MODULO_BAROMETRO_BMP180.cpp
+++ src/MODULO_BAROMETRO_BMP180.cpp
@@ -1,5 +1,6 @@
 #include <M5StickCPlus2.h>
 #include <Wire.h>
+#include <SFE_BMP180.h>
 #include "submenu.h"
 #include "const.h"
 
@@ -8,93 +9,78 @@
 #define BYTES 2
 #define BMP180_DIR 0x77
 
+SFE_BMP180 bmp;
+double temp;
+
 void inicializarBMP180()
 {
-    Wire.beginTransmission(BMP180_DIR);
-    Wire.write(0xAA); // Dirección del primer registro de calibración
-    Wire.endTransmission();
-
-    Wire.requestFrom(BMP180_DIR, 22); // Leer 22 bytes de los registros de calibración
-
-    if (Wire.available() == 22)
+    M5.Lcd.setTextSize(2);
+    Serial.begin(9600);
+    limpiarPantalla(10, 10);
+    Wire.begin(SDA_PIN, SCL_PIN);
+    delay(1000);
+    if (!bmp.begin())
     {
-        // Lectura de los registros de calibración
-        int AC1 = (Wire.read() << 8) | Wire.read();
-        int AC2 = (Wire.read() << 8) | Wire.read();
-        int AC3 = (Wire.read() << 8) | Wire.read();
-        unsigned int AC4 = (Wire.read() << 8) | Wire.read();
-        unsigned int AC5 = (Wire.read() << 8) | Wire.read();
-        unsigned int AC6 = (Wire.read() << 8) | Wire.read();
-        int B_1 = (Wire.read() << 8) | Wire.read();
-        int B2 = (Wire.read() << 8) | Wire.read();
-        int MB = (Wire.read() << 8) | Wire.read();
-        int MC = (Wire.read() << 8) | Wire.read();
-        int MD = (Wire.read() << 8) | Wire.read();
+        limpiarPantalla(0, 60);
+        M5.Lcd.print("No se encontro dispositivo");
+        modulo = MENU_SENSORES;
+        delay(4000);
+    }
+}
 
-        // Puedes guardar estos valores en variables globales o en una estructura
-        Serial.println("Registros de calibración leídos:");
-        Serial.print("AC1: ");
-        Serial.println(AC1);
-        Serial.print("AC2: ");
-        Serial.println(AC2);
-        Serial.print("AC3: ");
-        Serial.println(AC3);
-        Serial.print("AC4: ");
-        Serial.println(AC4);
-        Serial.print("AC5: ");
-        Serial.println(AC5);
-        Serial.print("AC6: ");
-        Serial.println(AC6);
-        Serial.print("B1: ");
-        Serial.println(B_1);
-        Serial.print("B2: ");
-        Serial.println(B2);
-        Serial.print("MB: ");
-        Serial.println(MB);
-        Serial.print("MC: ");
-        Serial.println(MC);
-        Serial.print("MD: ");
-        Serial.println(MD);
-    }
-    else
-    {
-        Serial.println("Error al leer los registros de calibración.");
-    }
+void printTemperatura()
+{
+    limpiarPantalla(0, 10);
+    M5.Lcd.println("Temp");
+    M5.Lcd.print(temp, 2);
+    M5.Lcd.println(" C");
+    M5.Lcd.print((9.0 / 5.0) * temp + 32.0, 2);
+    M5.Lcd.print(" F");
+    delay(100);
 }
 
 void MODULO_BAROMETRO()
 {
-    Serial.begin(9600);
-    Wire.begin(SDA_PIN, SCL_PIN);
-    limpiarPantalla(10, 10);
-    delay(1000);
-    Serial.print("--- INICIANDO ---");
-
     inicializarBMP180();
 
-    while (!mirarBtnIrAtras(MENU_SENSORES))
+    while (modulo == BAROMETRO)
     {
-        Wire.beginTransmission(BMP180_DIR);
-        Wire.write(0x2E);
-        Wire.endTransmission();
-        delay(7);
+        int status = bmp.startTemperature();
 
-            Wire.requestFrom(BMP180_DIR, 2);
+        if (status != 0)
+        {
+            delay(5);
+
+            status = bmp.getTemperature(temp);
+            if (status != 0)
+            {
+                printTemperatura();
+            }
+        }
+        // Wire.beginTransmission(BMP180_DIR);
+        // Wire.write(0xF6); // 0x2E para leer temperatura
+        // Wire.endTransmission();
+        // delay(7);
+
+        // Wire.requestFrom(BMP180_DIR, 2);
         // if (Wire.available() == 2)
         // {
-        int data = Wire.read();
-        int data2 = Wire.read();
+        // int data = Wire.read();
+        // int data2 = Wire.read();
 
-        Serial.print("Temp1= ");
-        Serial.println(data);
-        Serial.print("Temp2= ");
-        Serial.println(data);
+        // limpiarPantalla(0, 10);
+        // M5.Lcd.print("Temp1 = ");
+        // M5.Lcd.println(data);
+        // M5.Lcd.print("Temp2 = ");
+        // M5.Lcd.println(data);
         // }
         // else
         // {
         // Serial.println("No se recibieron suficientes datos.");
         // }
 
-        delay(3000); // Espera un segundo antes de la próxima lectura
+        // delay(3000); // Espera un segundo antes de la próxima lectura
+        mirarBtnIrAtras(MENU_SENSORES);
     }
+    M5.Lcd.setTextSize(1.1);
 }
