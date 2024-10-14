95f9d365679a265984f1396b4af02435b0a7cd94
mendezfreitez <mendezfreitez@gmail.com>
2024-10-14 08:49:19 -0300

    Cambios varios

--- src/MODULO_NFC.cpp
+++ src/MODULO_NFC.cpp
@@ -6,7 +6,11 @@
 
 Adafruit_PN532 nfc(26, 25);
 String tagId = "None", dispTag = "None";
-// byte nuidPICC[4];
+uint8_t idTarjeta[] = {0, 0, 0, 0, 0, 0, 0};
+uint8_t idTarjetaLong;
+bool success;
+bool resp;
+int bloqueNro;
 
 bool setup_nfc()
 {
@@ -27,7 +31,6 @@ bool setup_nfc()
   else
   {
     limpiarPantalla(0, 10);
-    // M5.Lcd.print(version);
     Serial.print(version);
     Serial.print("Chip encontrado: PN5");
     Serial.println((version >> 24) & 0xFF, HEX);
@@ -46,103 +49,73 @@ bool setup_nfc()
   }
 }
 
-String tagToString(byte id[4])
+void imprimirHex(const byte *id, int dataLong)
 {
-  String tagId = "";
-  for (byte i = 0; i < 4; i++)
+  uint32_t szPos;
+  for (szPos = 0; szPos < dataLong; szPos++)
   {
-    if (i < 3)
-      tagId += String(id[i]) + ".";
-    else
-      tagId += String(id[i]);
+    if (id[szPos] <= 0xF)
+    {
+      Serial.print(F("0"));
+      M5.Lcd.print(F("0"));
+    }
+    Serial.print(id[szPos], HEX);
+    M5.Lcd.print(id[szPos], HEX);
+
+    if ((dataLong > 1) && (szPos != dataLong - 1))
+    {
+      Serial.print(F(" "));
+      M5.Lcd.print(F(" "));
+    }
   }
-  return tagId;
+  Serial.println();
+  M5.Lcd.println();
 }
 
-void leerPaginas()
+void leerPaginas(int nroBytes)
 {
-  // uint8_t bufferPagina[4];
-  // int paginas = 48;
-  // for (int p = 0; p < paginas; p++)
-  // {
-  //   Serial.println(nfc.ntag2xx_ReadPage(p, bufferPagina));
-  //   if (nfc.ntag2xx_ReadPage(p, bufferPagina) == 1)
-  //   {
-  //     Serial.print("P ");
-  //     Serial.print(p);
-  //     Serial.print("= ");
-  //     for (int i = 0; i < sizeof(bufferPagina); i++)
-  //     {
-  //       Serial.print(bufferPagina[i], HEX);
-  //       Serial.print(":");
-  //     }
-  //     Serial.println("");
-  //   }
-  // }
-  uint8_t apdubuffer[255] = {}, apdulen = 0;
-  // uint8_t ppse[] = {0x8E, 0x6F, 0x23, 0x84, 0x0E, 0x32, 0x50, 0x41, 0x59, 0x2E, 0x53, 0x59, 0x53, 0x2E, 0x44, 0x44, 0x46, 0x30, 0x31, 0xA5, 0x11, 0xBF, 0x0C, 0x0E, 0x61, 0x0C, 0x4F, 0x07, 0xA0, 0x00, 0x00, 0x00, 0x03, 0x10, 0x10, 0x87, 0x01, 0x01, 0x90, 0x00};
-  nfc.AsTarget();
-  (void)nfc.getDataTarget(apdubuffer, &apdulen); // Read initial APDU
-  if (apdulen > 0)
+  uint8_t keya[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+  uint8_t data[nroBytes];
+  if (nroBytes == 16)
   {
-    for (uint8_t i = 0; i < apdulen; i++)
+    resp = nfc.mifareclassic_AuthenticateBlock(idTarjeta, idTarjetaLong, bloqueNro, 0, keya);
+    if (resp)
     {
-      Serial.print(" 0x");
-      Serial.print(apdubuffer[i], HEX);
+      nfc.mifareclassic_ReadDataBlock(bloqueNro, data);
     }
-    Serial.println("");
   }
+  else
+  {
+    nfc.mifareultralight_ReadPage(bloqueNro, data);
+  }
+
+  Serial.printf("Bloque: %i\n", bloqueNro);
+  imprimirHex(data, nroBytes);
+  Serial.println("");
+  delay(1000);
 }
 
 void lecturaTarjeta()
 {
-  bool success;
-  uint8_t idTarjeta[] = {0, 0, 0, 0, 0, 0, 0};
-
-  uint8_t idTarjetaLong;
-  byte nuidPICC[idTarjetaLong];
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &idTarjeta[0], &idTarjetaLong);
 
   if (success)
   {
     destello(GREEN, 200);
-    // Serial.print("Valor: ");
-    // Serial.println(idTarjeta);
-    Serial.print("Longitud: ");
     Serial.print(idTarjetaLong, DEC);
     Serial.println(" bytes");
 
     limpiarPantalla(0, 10);
-    M5.Lcd.print("Longitud: ");
+    M5.Lcd.print("");
     M5.Lcd.print(idTarjetaLong, DEC);
     M5.Lcd.println(" bytes");
-    // Serial.print("UID Value: "
+    M5.Lcd.printf("%.0f posiciones\n", pow(2, idTarjetaLong));
 
-    String tagId = "";
+    imprimirHex(idTarjeta, idTarjetaLong);
 
-    Serial.println("-----------------");
-    for (uint8_t i = 0; i < idTarjetaLong; i++)
-    {
-      Serial.println(idTarjeta[i], HEX);
-      nuidPICC[i] = idTarjeta[i];
-      // Serial.print(" "); Serial.print(idTarjeta[i], DEC);
-      if ((i + 1) < idTarjetaLong)
-        tagId += String(idTarjeta[i]) + ":";
-      else
-        tagId += String(idTarjeta[i]);
-    }
-    Serial.println("-----------------");
-    // tagId = tagToString(nuidPICC);
-    // dispTag = tagId;
-    Serial.print(F("Etiqueta: "));
-    Serial.println(tagId);
-    Serial.println();
-
-    M5.Lcd.print(F("Etiqueta: "));
-    M5.Lcd.println(tagId);
-
-    leerPaginas();
-    delay(1000); // 1 second halt
+    M5.Lcd.setTextSize(1.1);
+    leerPaginas(idTarjetaLong == 4 ? 16 : 32);
+    delay(1000);
   }
   else
   {
