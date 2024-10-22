dea87cc535bd37653293df6bd05a0a8e2938ffb9
mendezfreitez <mendezfreitez@gmail.com>
2024-10-22 07:27:50 -0300

    Borrado de libreria que al parecer servia pero no.

--- src/MODULO_RADIOFRECUENCIA.cpp
+++ src/MODULO_RADIOFRECUENCIA.cpp
@@ -1,13 +1,10 @@
 #include <M5StickCPlus2.h>
 #include <submenu.h>
 #include <const.h>
-#include <CC1101_RF.h>
 
-CC1101 radio;
 
 void MODULO_RADIOFRECUENCIA()
 {
-    radio.begin(433);
     limpiarPantalla(10, 60);
     M5.Lcd.print("EN DESAROLLO");
     while (!mirarBtnIrAtras(MENU_SENSORES))
